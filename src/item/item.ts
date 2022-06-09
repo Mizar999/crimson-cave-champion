7d3baeb3b9460efd24e7f1657ceb576b5008289d
Mario <mizar2502@aol.com>
2022-06-09 20:35:48 +0200

    Add item template

--- src/item/item.ts
+++ src/item/item.ts
@@ -1,4 +1,6 @@
 import { Attack } from "../system/attack";
+import { Actor } from "../actor/actor";
+import { Entity } from "../ui/entity";
 
 export const enum SlotType {
     None,
@@ -16,5 +18,39 @@ export class Item {
     private armorClass: number;
     private armorClassModifier: number;
     private weight: number;
-    // private effect: Effect; // TODO implement effects
+    private throwRange: number;
+    // Use, throw, effect?
+}
+
+// TODO implement classes
+export class Use {
+    prepare: (source: Actor) => boolean; // Item argument?
+    onUse: (source) => boolean;
+    onAfterUse: () => void;
+
+    use(source: Actor): void {
+        if (this.prepare(source)) {
+            if (this.onUse(source)) {
+                this.onAfterUse();
+            }
+        }
+    }
+}
+
+export class Effect { // Status?
+    onStartOfTurn: () => void;
+    onEndOfTurn: () => void;
+}
+
+export class Throw {
+    hits: (source: Actor) => Entity[];
+    onHit: (source, targets: Entity[]) => void;
+
+    throw(source: Actor) {
+        const targets = this.hits(source);
+        if (targets) {
+            this.onHit(source, targets);
+        }
+    }
+    
 }
