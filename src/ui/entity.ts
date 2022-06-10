f471d3b4b8188e30e269f67f3c6a7072cbbc161d
Mario <mizar2502@aol.com>
2022-06-10 07:48:29 +0200

    Move block type to entity file

--- src/ui/entity.ts
+++ src/ui/entity.ts
@@ -1,5 +1,6 @@
 import {Visual} from "./visual";
-import {BlockType} from "./map/block-type";
+
+export type BlockType = "BlocksNone" | "BlocksMovement";
 
 export class Entity {
     constructor(public readonly visual: Visual, public blocks: BlockType = "BlocksNone") { }
